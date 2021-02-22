9ee71600233f4731bc7320af6deee3fd232434e9
Gautier Arcin <gautier.arcin@gmail.com>
2021-02-22 10:15:21 +0100

    Added dilatation of Occupency grid for path planning

--- scripts/path_planner.py
+++ scripts/path_planner.py
@@ -6,10 +6,17 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 import math
 
+
 from nav_msgs.msg import MapMetaData
 from nav_msgs.msg import OccupancyGrid
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
+
+import cv2
+
+import sys
+import argparse
+from jetbrain_path import PRMController, Obstacle, Utils
 
 
 
@@ -31,7 +38,8 @@ class Planner:
 		self.endPoint = None
 		self.endOrientation = None
 
-
+		self.robotFootprint = 0.160
+		#self.robotFootprint = 2
 
 		#RosCallback
 		rospy.init_node('jetbrain_path_planner', anonymous=True)
@@ -46,7 +54,7 @@ class Planner:
 		log = ": start point updated, point : " + str(self.startPoint) 
 		
 		# Quaternion utilization defined "" just in case "".
-		# Because the robot is spherical,it's not used in the code thoo
+		# Because the robot is spherical, it's not used in the planenr code thoo
 		try:
 			self.startOrientation =  R.from_quat([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
 			log += ", quaternion : " + str(self.startOrientation.as_quat())
@@ -62,7 +70,7 @@ class Planner:
 		log = ": end point updated, point : " + str(self.endPoint) 
 
 		# Quaternion utilization defined "" just in case "".
-		# Because the robot is spherical,it's not used in the code thoo
+		# Because the robot is spherical, it's not used in the planner code thoo
 		try:
 			self.endOrientation =  R.from_quat([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
 			log += ", quaternion : " + str(self.endOrientation.as_quat())
@@ -76,14 +84,49 @@ class Planner:
 	def callback_OGrid(self, msg):
 		self.oGrid = np.array(msg.data).reshape((msg.info.height, msg.info.width))
 		self.oGridOrigin = np.array([msg.info.origin.position.x, msg.info.origin.position.y])
-
-		self.oGridRes = msg.info.resolution
+		
+		self.oGridCPM = 1 / msg.info.resolution
 		self.oGridWidth = msg.info.width
 		self.oGridHeight = msg.info.height
 
+		#if self.oGrid is not None: 
+		# Get opencv-ready image from current ogrid (255 is occupied, 0 is clear)
+		oGridThreeshold = 90
+		occImg = 255*np.greater(self.oGrid, oGridThreeshold).astype(np.uint8)
+
+		# Dilate the image
+		robotPix = int(self.oGridCPM*self.robotFootprint)
+		robotPix += robotPix%2
+		occImgDil = cv2.dilate(occImg, np.ones((robotPix, robotPix), np.uint8))
+		cv2.imshow("Dilated map", occImgDil)
+		cv2.waitKey(0)
+
 		rospy.loginfo(rospy.get_caller_id() + ": Occupency Grid updated, shape : " + str(self.oGrid.shape) + ", origin : " + str(self.oGridOrigin) )
+
+		# Generating Ogrid Image through OpenCV + diltation for path-planning
+		# Inspired by this library: https://github.com/jnez71/lqRRT
+
+		"""
+		# Construct the initial sample space and get bounds in pixel coordinates
+		ss = self.behavior.gen_ss(self.next_seed, self.goal)
+		pmin = self.intup(self.oGridCPM * ([ss[0][0], ss[1][0]] - self.ogrid_origin))
+		pmax = self.intup(self.oGridCPM * ([ss[0][1], ss[1][1]] - self.ogrid_origin))
+
+		# Other quantities in pixel coordinates
+		seed = self.intup(self.oGridCPM * (self.next_seed[:2] - self.ogrid_origin))
+		goal = self.intup(self.oGridCPM * (self.goal[:2] - self.ogrid_origin))
+		step = int(self.oGridCPM * params.ss_step)
+
+		# Make sure seed and goal are physically meaningful
+		try:
+			occ_img_dial[seed[1], seed[0]]
+			occ_img_dial[goal[1], goal[0]]
+		except IndexError:
+			print("Goal and/or seed out of bounds of occupancy grid!")
+			return(0, escape.gen_ss(self.next_seed, self.goal, 1), np.copy(self.goal), False)"""
+
+
 
 
 if __name__ == '__main__':
 	test = Planner()
-
