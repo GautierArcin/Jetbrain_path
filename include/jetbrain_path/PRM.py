dfc222fe9141261d49497c5fc8fd30464efa7159
Gautier Arcin <gautier.arcin@gmail.com>
2021-03-01 13:53:53 +0100

    working prm

---
+++ include/jetbrain_path/PRM.py
@@ -0,0 +1,300 @@
+
+import random
+import math
+import time
+import os
+import cv2
+import numpy as np
+from scipy.spatial import cKDTree
+
+class Node:
+    """
+    Node class for dijkstra search algorithm
+    """
+
+    def __init__(self, x, y, cost, parent_index):
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.parent_index = parent_index
+
+    def __str__(self):
+        return "x: " + str(self.x) + ", y: " + str(self.y) + "," + str(self.cost) + "," + str(self.parent_index)
+
+
+class PRM:
+    """
+    PRM Planner
+
+    """
+
+    def __init__(self, image, res, sx, sy, gx, gy, robotSize, NSample=500, edgeFromeOneSamplePoint=20, maxEdgeLength=10.0):
+        self._image = image
+        self._res = res
+
+        self._sx = sx
+        self._sy = sy
+        self._gx = gx
+        self._gy = gy
+        self._robotSize = robotSize
+
+        self._edgeFromeOneSamplePoint = edgeFromeOneSamplePoint
+        self._NSample = NSample
+        self._maxEdgeLength = maxEdgeLength
+
+    def __str__(self):
+        return "Prm planner with parameters : \nNumber Sample : " + str(self._NSample) + ", Maximum edge from one point : " + str(self._edgeFromeOneSamplePoint) + ", Maximum edge length : " + str(self._maxEdgeLength)
+
+    def getObstaclesFromImage(self):
+        ox = []
+        oy = []
+        for i in range(len(self._image)):
+            for j in range(len(self._image[i])):
+                if self._image[i][j] == 255:
+                    # Conversion from openCV frame to real world frame
+                    oy.append((len(self._image) - i)/self._res)
+                    ox.append(j/self._res)
+        return ox, oy
+
+    def startPlanner(self):
+
+        ox, oy = self.getObstaclesFromImage()
+
+        obstacle_kd_tree = cKDTree(np.vstack((ox, oy)).T)
+
+        sample_x, sample_y = self.generateSamplePoints(
+            ox, oy, obstacle_kd_tree)
+
+        road_map = self.generateRoadMap(
+            sample_x, sample_y, obstacle_kd_tree)
+
+        rx, ry = self.dijkstra(road_map, sample_x, sample_y)
+
+        return rx, ry
+
+    def isCollision(self, sx, sy, gx, gy, obstacle_kd_tree):
+        x = sx
+        y = sy
+        dx = gx - sx
+        dy = gy - sy
+        yaw = math.atan2(dy, dx)
+        d = math.hypot(dx, dy)
+
+        if d > self._maxEdgeLength:
+            return True # not checking if collision or not
+
+        D = self._robotSize
+        n_step = round(d / D)
+
+        for i in range(int(n_step)):
+            dist, _ = obstacle_kd_tree.query([x, y])
+            if dist <= self._robotSize:
+                return True  # it's a collision
+            x += D * math.cos(yaw)
+            y += D * math.sin(yaw)
+
+        # goal point check
+        dist, _ = obstacle_kd_tree.query([self._gx, self._gy])
+        if dist <= self._robotSize:
+            return True  # it's a collision
+
+        return False # it's not a collision
+
+    def generateRoadMap(self, sample_x, sample_y, obstacle_kd_tree):
+
+        sample_kd_tree = cKDTree(np.vstack((sample_x, sample_y)).T)
+        road_map = []
+        nSample = len(sample_x)
+
+        for (i, ix, iy) in zip(range(nSample), sample_x, sample_y):
+
+            dists, indexes = sample_kd_tree.query([ix, iy], k=nSample)
+            edge_id = []
+
+            for ii in range(1, len(indexes)):
+                nx = sample_x[indexes[ii]]
+                ny = sample_y[indexes[ii]]
+
+                if not self.isCollision(ix, iy, nx, ny, obstacle_kd_tree):
+                    edge_id.append(indexes[ii])
+
+                if len(edge_id) >= self._edgeFromeOneSamplePoint:
+                    break
+
+            road_map.append(edge_id)
+
+        #  plot_road_map(road_map, sample_x, sample_y)
+
+        return road_map
+
+    def dijkstra(self, road_map, sample_x, sample_y):
+
+        goal_node = Node(self._gx, self._gy, 0.0, -1)
+        start_node = Node(self._sx, self._sy, 0.0, -1)
+
+        open_set, closed_set = dict(), dict()
+        open_set[len(road_map) - 2] = start_node
+        path_found = True
+
+        while True:
+            if not open_set:
+                path_found = False
+                break
+
+            c_id = min(open_set, key=lambda o: open_set[o].cost)
+            current = open_set[c_id]
+
+            if c_id == (len(road_map) - 1):
+                goal_node.parent_index = current.parent_index
+                goal_node.cost = current.cost
+                break
+
+            # Remove the item from the open set and add it to the closed set
+            del open_set[c_id]
+            closed_set[c_id] = current
+
+            # expand search grid based on motion model
+            for i in range(len(road_map[c_id])):
+                n_id = road_map[c_id][i]
+                dx = sample_x[n_id] - current.x
+                dy = sample_y[n_id] - current.y
+                d = math.hypot(dx, dy)
+                node = Node(sample_x[n_id], sample_y[n_id],
+                            current.cost + d, c_id)
+
+                if n_id in closed_set:
+                    continue
+                # Otherwise if it is already in the open set
+                if n_id in open_set:
+                    if open_set[n_id].cost > node.cost:
+                        open_set[n_id].cost = node.cost
+                        open_set[n_id].parent_index = c_id
+                else:
+                    open_set[n_id] = node
+
+        if path_found is False:
+            return [], []
+
+        # generate final course
+        rx, ry = [goal_node.x], [goal_node.y]
+        parent_index = goal_node.parent_index
+        while parent_index != -1:
+            n = closed_set[parent_index]
+            rx.append(n.x)
+            ry.append(n.y)
+            parent_index = n.parent_index
+
+        return rx, ry
+
+    def generateSamplePoints(self, ox, oy, obstacle_kd_tree):
+        max_x = max(ox)
+        max_y = max(oy)
+        min_x = min(ox)
+        min_y = min(oy)
+
+        sample_x, sample_y = [], []
+
+        while len(sample_x) <= self._NSample:
+            tx = (random.random() * (max_x - min_x)) + min_x
+            ty = (random.random() * (max_y - min_y)) + min_y
+
+            dist, index = obstacle_kd_tree.query([tx, ty])
+
+            if dist >= self._robotSize:
+                sample_x.append(tx)
+                sample_y.append(ty)
+
+        sample_x.append(self._sx)
+        sample_y.append(self._sy)
+
+        sample_x.append(self._gx)
+        sample_y.append(self._gy)
+
+        return sample_x, sample_y
+
+    def pointMapToImg(self, xMap, yMap):
+        xPixel = int(xMap*self._res)
+        yPixel = int(len(self._image) - int(yMap*self._res))
+        return (xPixel, yPixel)
+
+    def saveToVideo(self, rx, ry, converToGif=False, startIconSize=2, circleSize=2, lineSize=2):
+        # RGB conversion
+        imageStart = cv2.cvtColor(self._image, cv2.COLOR_GRAY2BGR)
+
+        #imageStart = self._image
+
+        height, width, channel = imageStart.shape
+
+        fourcc = cv2.VideoWriter_fourcc(*'MP42')
+        video = cv2.VideoWriter('./output.avi', fourcc, 2, (width, height))
+
+        # Start and end rectangle (green and red)
+        # StartIconSize define the size of each rectangle in pixel
+        cv2.rectangle(imageStart,
+                      self.pointMapToImg(
+                          self._sx-startIconSize/self._res, self._sy-startIconSize/self._res),
+                      self.pointMapToImg(
+                          self._sx+startIconSize/self._res, self._sy+startIconSize/self._res),
+                      (0, 255, 0), -1)
+
+        cv2.rectangle(imageStart,
+                      self.pointMapToImg(self._gx-startIconSize/self._res, self._gy -
+                                         startIconSize/self._res),
+                      self.pointMapToImg(self._gx+startIconSize/self._res, self._gy +
+                                         startIconSize/self._res),
+                      (0, 0, 255), -1)
+
+        # Wiriting first image multiple time in order to have a start "wait"
+        for i in range(6):  # 3 seconds of wait
+            video.write(imageStart)
+
+        
+        if(rx != None):
+            imageSubsequent = imageStart
+            for i in range(len(rx)-2, 0, -1):
+                cv2.circle(imageStart,
+                           self.pointMapToImg(rx[i], ry[i]),
+                           circleSize,
+                           (255, 0, 0), -1)
+                cv2.line(imageStart,
+                         self.pointMapToImg(
+                             rx[i+1], ry[i+1]),
+                         self.pointMapToImg(rx[i], ry[i]),
+                         (255, 0, 0), 2)
+                video.write(imageSubsequent)
+
+            i = 0
+            cv2.line(imageStart,
+                     self.pointMapToImg(rx[i+1], ry[i+1]),
+                     self.pointMapToImg(rx[i], ry[i]),
+                     (255, 0, 0), lineSize)
+            video.write(imageSubsequent)
+
+        video.release()
+
+        # Convert to gif
+        # https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
+        if(converToGif):
+            filePathIn = "output.avi"
+            filePathOut = "output.gif"
+            command = "ffmpeg -y -i " + \
+                str(filePathIn) + \
+                " -filter_complex \"[0:v] split [a][b];[a] palettegen [p];[b][p] paletteuse\" " + str(
+                    filePathOut)
+            os.system(command)
+
+
+if __name__ == "__main__":
+
+    img = cv2.imread(__file__.replace("prm.py", "input2.png"), 0)
+    res = 20.0
+    sx = 16.6
+    sy = 1.6
+    gx = 265.0 / 20  # [m]
+    gy = 220.0 / 20  # [m]
+    robotSize = 0.160 / 2  # [m]
+
+    test = PRM(img, res, sx, sy, gx, gy, robotSize)
+    rx, ry = test.startPlanner()
+    test.saveToVideo(rx, ry, True)
+
--- scripts/path_planner.py
+++ scripts/path_planner.py
@@ -2,31 +2,36 @@
 
 import rospy
 
-import numpy as np
-from scipy.spatial.transform import Rotation as R
-import math
-
-
 from nav_msgs.msg import MapMetaData
 from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import Path
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
 
+from scipy.spatial.transform import Rotation as R
 import cv2
+import numpy as np
+import math
 
+import tf
+
+from jetbrain_path import PRM
 
 
 class Planner:
+
 	def __init__(self):
 		# Occupency Grid
 		#
 		# Note: we consider only 2d case, so we only take only into account x/y
 		# Also orientation is not considered because the robot is considered spherical
+
 		self.oGrid = None 
 		self.oGridOrigin = None
-		self.oGridRes = None
+		self.oGridCPM = None
 		self.oGridWidth = None
 		self.oGridHeight = None
+		self.image=  None
 
 		self.startPoint = None
 		self.startOrientation = None
@@ -35,33 +40,116 @@ class Planner:
 		self.endOrientation = None
 
 		self.robotFootprint = 0.160
-		#self.robotFootprint = 2
+
+		#Config on planner
+		self.path = None
+		self.plannerNSample = 500 
+		self.plannerEdgeFromeOneSamplePoint=20
+		self.maxEdgeLength=10.0
+
 
 		#RosCallback
 		rospy.init_node('jetbrain_path_planner', anonymous=True)
+
 		rospy.Subscriber('map', OccupancyGrid, self.callback_OGrid)
 		rospy.Subscriber('start', PoseStamped, self.callback_startPos)
 		rospy.Subscriber('end', PoseStamped, self.callback_endPos)
-		rospy.spin() 
+		self.pub = rospy.Publisher('/path', Path, latch=True)
+		# https://stackoverflow.com/questions/40508651/writing-a-ros-node-with-both-a-publisher-and-subscriber
+		timer = rospy.Timer(rospy.Duration(0.5), self.callback_timer)
+		rospy.spin()
+		timer.shutdown()
+
+
 
 	def isPlannerExecutable(self):
 		returnBool = True
 
 		# Checking if we're lacking data to execute planner
-		if startPoint is None:
+		if self.startPoint is None:
 			returnBool = False
-			rospy.log(rospy.get_caller_id() + ": Can't execute planner, start point is lacking")
-		if endPoint is None:
+			rospy.loginfo("Can't execute planner, start point is lacking")
+		if self.endPoint is None:
 			returnBool = False
-			rospy.log(rospy.get_caller_id() + ": Can't execute planner, end point is lacking")
-		if oGrid is None:
+			rospy.loginfo("Can't execute planner, end point is lacking")
+		if self.oGrid is None:
 			returnBool = False
-			rospy.log(rospy.get_caller_id() + ": Can't execute planner, map is lacking")
+			rospy.loginfo("Can't execute planner, map is lacking")
 
-		#  @ Check if start or end point outside dimension of current map
+		# @todo Check if start or end point outside dimension of current map
 
 		return returnBool
 
+
+
+	def executePlanner(self):
+		if(self.isPlannerExecutable()):
+
+			sx = self.startPoint[0]  # [m]
+			sy = self.startPoint[1]  # [m]
+			gx = self.endPoint[0]  # [m]
+			gy = self.endPoint[1]  # [m]
+			robotSize = self.robotFootprint / 2  # [m]
+
+			messageToLog =  "calling Prm planner with : "
+			messageToLog += "Sp : " + str(sx) + ", " + str(sy)
+			messageToLog += ", Gp : " + str(gx) + ", " + str(gy)
+			messageToLog += ", Rz : " + str(robotSize) 
+			messageToLog += ", CPM : " + str(round(self.oGridCPM))
+			rospy.loginfo(messageToLog)
+
+			beforeTime = rospy.Time.now()
+			Planner = PRM(self.image , round(self.oGridCPM), sx, sy, gx, gy, robotSize, self.plannerNSample, self.plannerEdgeFromeOneSamplePoin, self.maxEdgeLength)
+			rx, ry = Planner.startPlanner()
+			afterTime = rospy.Time.now()
+
+			difference = afterTime.secs - beforeTime.secs
+			difference += (afterTime.nsecs - beforeTime.nsecs) * 0.000000001
+
+			if rx is not None:
+				Planner.saveToVideo(rx, ry, True)
+				npRx = np.asarray(rx)
+				npRy = np.asarray(ry)
+				stacked = np.stack((npRx,npRy),-1) 
+
+				msg = Path()
+				msg.header.frame_id = "/path"
+				msg.header.stamp = rospy.Time.now()
+				for i in range(len(stacked)-1,-1,-1):
+					print(stacked[i])
+					pose = PoseStamped()
+
+					#Adapting points to frame of Rviz
+					pose.pose.position.x = (self.oGridWidth / (self.oGridCPM)) - stacked[i][0]
+					pose.pose.position.y = (self.oGridHeight / (self.oGridCPM)) - stacked[i][1]
+
+
+					pose.pose.position.z = 0
+					pose.pose.orientation.x = 0
+					pose.pose.orientation.y = 0
+					pose.pose.orientation.z = 0
+					pose.pose.orientation.w = 1
+					msg.poses.append(pose)
+				self.path = msg
+				message = "goal found ! in " + str( difference ) +  "s"
+				rospy.loginfo(message)
+			else:
+				message = "goal not found in " + str( difference) + "s... Try to change points or parameter of the planner."
+				rospy.logwarn(message)
+
+
+
+
+
+	def callback_timer(self, msg):
+
+		if self.oGrid is not None:
+			br = tf.TransformBroadcaster()
+			#br.sendTransform(( - self.oGridOrigin[0], - self.oGridOrigin[1], 0.0), (0.0, 0.0, 0.0, 1), rospy.Time.now(), "map", "/path")
+			br.sendTransform(( 0.0, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0), rospy.Time.now(), "map", "/path")
+
+		if(self.path != None):
+			self.pub.publish(self.path)
 
 	def callback_startPos(self, msg):
 		self.startPoint = np.array([msg.pose.position.x, msg.pose.position.y])
@@ -78,6 +166,8 @@ class Planner:
 			log += ", quaternion : " + str(self.startOrientation)
 
 		rospy.loginfo(rospy.get_caller_id() + log)
+		self.executePlanner()
+
 
 	def callback_endPos(self, msg):
 		self.endPoint = np.array([msg.pose.position.x, msg.pose.position.y])
@@ -94,6 +184,8 @@ class Planner:
 			log += ", quaternion : " + str(self.endOrientation)
 
 		rospy.loginfo(rospy.get_caller_id() + log)
+		self.executePlanner()
+
 
 	def callback_OGrid(self, msg):
 		# Generating Ogrid Image through OpenCV + diltation for path-planning
@@ -102,7 +194,7 @@ class Planner:
 		self.oGrid = np.array(msg.data).reshape((msg.info.height, msg.info.width))
 		self.oGridOrigin = np.array([msg.info.origin.position.x, msg.info.origin.position.y])
 		
-		self.oGridCPM = 1 / msg.info.resolution
+		self.oGridCPM = 1/ msg.info.resolution
 		self.oGridWidth = msg.info.width
 		self.oGridHeight = msg.info.height
 
@@ -111,47 +203,23 @@ class Planner:
 		oGridThreeshold = 90
 		occImg = 255*np.greater(self.oGrid, oGridThreeshold).astype(np.uint8)
 
-		# Dilate the image
-		robotPix = int(self.oGridCPM*self.robotFootprint)
-		robotPix += robotPix%2
-		occImgDil = cv2.dilate(occImg, np.ones((robotPix, robotPix), np.uint8))
-
 		#Flip the image (Might be Uncessary, did it to have the same orientation as Rviz vizualization)
-		occImgDilFlip = cv2.flip(occImgDil, 1)
+		occImgFlip = cv2.flip(occImg, 1)
 
-		cv2.imshow("Map", occImg)
-		cv2.imshow("Dilated map", occImgDil)
-		cv2.imshow("Dilated map Flipped", occImgDilFlip)
-		cv2.waitKey(0)
+		#cv2.imshow("Map", occImg)
+		#cv2.imshow("Map Flipped", occImgFlip)
+		#cv2.waitKey(0)
 
+		self.image = occImgFlip
 
 		height, width  = occImg.shape 
 
-		rospy.loginfo(rospy.get_caller_id() + ": Occupency Grid updated, shape : " + str(self.oGrid.shape) + ", origin : " + str(self.oGridOrigin) )
+		rospy.loginfo(rospy.get_caller_id() + ": Occupency Grid updated, shape : " + str(self.oGrid.shape) + ", origin : " + str(self.oGridOrigin) + ", res : " + str(msg.info.resolution) )
 		rospy.loginfo(rospy.get_caller_id() + ": Image shape : " + str(height) + ", " + str(width) )
 
 
-		"""
-		# Construct the initial sample space and get bounds in pixel coordinates
-		ss = self.behavior.gen_ss(self.next_seed, self.goal)
-		pmin = self.intup(self.oGridCPM * ([ss[0][0], ss[1][0]] - self.ogrid_origin))
-		pmax = self.intup(self.oGridCPM * ([ss[0][1], ss[1][1]] - self.ogrid_origin))
-
-		# Other quantities in pixel coordinates
-		seed = self.intup(self.oGridCPM * (self.next_seed[:2] - self.ogrid_origin))
-		goal = self.intup(self.oGridCPM * (self.goal[:2] - self.ogrid_origin))
-		step = int(self.oGridCPM * params.ss_step)
-
-		# Make sure seed and goal are physically meaningful
-		try:
-			occ_img_dial[seed[1], seed[0]]
-			occ_img_dial[goal[1], goal[0]]
-		except IndexError:
-			print("Goal and/or seed out of bounds of occupancy grid!")
-			return(0, escape.gen_ss(self.next_seed, self.goal, 1), np.copy(self.goal), False)"""
-
-
-
-
 if __name__ == '__main__':
-	test = Planner()
+	try:
+		Planner()
+	except rospy.ROSInterruptException:
+		pass
