580cc59b0385493a8de55c1acdd0e600f2db06e2
Gautier Arcin <gauta82@gmail.com>
2021-03-11 15:41:46 +0100

    measure speed

--- include/jetbrain_path/PRM.py
+++ include/jetbrain_path/PRM.py
@@ -7,9 +7,10 @@ import cv2
 import numpy as np
 from scipy.spatial import cKDTree
 
+
 class Node:
     """
-    Node class for dijkstra search algorithm
+    Node class for dijkstra / A* search algorithm
     """
 
     def __init__(self, x, y, cost, parent_index):
@@ -22,13 +23,14 @@ class Node:
         return "x: " + str(self.x) + ", y: " + str(self.y) + "," + str(self.cost) + "," + str(self.parent_index)
 
 
+
 class PRM:
     """
     PRM Planner
 
     """
 
-    def __init__(self, image, res, sx, sy, gx, gy, robotSize, NSample=500, edgeFromeOneSamplePoint=20, maxEdgeLength=10.0):
+    def __init__(self, image, res, sx, sy, gx, gy, robotSize, NSample=1000, edgeFromeOneSamplePoint=10, maxEdgeLength=10.0):
         self._image = image
         self._res = res
 
@@ -56,19 +58,34 @@ class PRM:
                     ox.append(j/self._res)
         return ox, oy
 
-    def startPlanner(self):
+    def startPlanner(self, aStar = True, verbiose=True):
 
+        start = time.clock()
         ox, oy = self.getObstaclesFromImage()
+        if(verbiose):
+            print(" Time get obstacle: " + str(time.clock()- start))
 
+        start = time.clock()
         obstacle_kd_tree = cKDTree(np.vstack((ox, oy)).T)
+        if(verbiose):
+            print(" Time get cdkTree: " + str(time.clock()- start))
 
+        start = time.clock()
         sample_x, sample_y = self.generateSamplePoints(
             ox, oy, obstacle_kd_tree)
+        if(verbiose):
+            print(" Time generate Sample Point: " + str(time.clock()- start))
 
+        start = time.clock()
         road_map = self.generateRoadMap(
             sample_x, sample_y, obstacle_kd_tree)
-
-        rx, ry = self.dijkstra(road_map, sample_x, sample_y)
+        if(verbiose):
+            print(" Time generate road map: " + str(time.clock()- start))
+        
+        start = time.clock()
+        rx, ry = self.AStar(road_map, sample_x, sample_y) if aStar else self.dijkstra(road_map, sample_x, sample_y)
+        if(verbiose):
+            print(" Time plan: " + str(time.clock()- start))
 
         return rx, ry
 
@@ -81,7 +98,7 @@ class PRM:
         d = math.hypot(dx, dy)
 
         if d > self._maxEdgeLength:
-            return True # not checking if collision or not
+            return True  # not checking if collision or not
 
         D = self._robotSize
         n_step = round(d / D)
@@ -98,7 +115,7 @@ class PRM:
         if dist <= self._robotSize:
             return True  # it's a collision
 
-        return False # it's not a collision
+        return False  # it's not a collision
 
     def generateRoadMap(self, sample_x, sample_y, obstacle_kd_tree):
 
@@ -106,28 +123,30 @@ class PRM:
         road_map = []
         nSample = len(sample_x)
 
+        print("Kd tree size / len")
+        print(sample_kd_tree)
+        print(sample_kd_tree.size)
+
         for (i, ix, iy) in zip(range(nSample), sample_x, sample_y):
 
             dists, indexes = sample_kd_tree.query([ix, iy], k=nSample)
             edge_id = []
 
-            for ii in range(1, len(indexes)):
-                nx = sample_x[indexes[ii]]
-                ny = sample_y[indexes[ii]]
+            for y in range(1, len(indexes)):
+                nx = sample_x[indexes[y]]
+                ny = sample_y[indexes[y]]
 
                 if not self.isCollision(ix, iy, nx, ny, obstacle_kd_tree):
-                    edge_id.append(indexes[ii])
+                    edge_id.append(indexes[y])
 
                 if len(edge_id) >= self._edgeFromeOneSamplePoint:
                     break
 
             road_map.append(edge_id)
 
-        #  plot_road_map(road_map, sample_x, sample_y)
-
         return road_map
 
-    def dijkstra(self, road_map, sample_x, sample_y):
+    def dijkstra(self, road_map, sample_x, sample_y, verbiose=True):
 
         goal_node = Node(self._gx, self._gy, 0.0, -1)
         start_node = Node(self._sx, self._sy, 0.0, -1)
@@ -142,6 +161,77 @@ class PRM:
                 break
 
             c_id = min(open_set, key=lambda o: open_set[o].cost)
+            current = open_set[c_id]
+
+            if c_id == (len(road_map) - 1):
+                goal_node.parent_index = current.parent_index
+                goal_node.cost = current.cost
+                break
+            
+
+            # Remove the item from the open set and add it to the closed set
+            del open_set[c_id]
+            closed_set[c_id] = current
+
+            # expand search grid based on motion model
+            for i in range(len(road_map[c_id])):
+                n_id = road_map[c_id][i]
+                dx = sample_x[n_id] - current.x
+                dy = sample_y[n_id] - current.y
+                d = math.hypot(dx, dy)
+                node = Node(sample_x[n_id], sample_y[n_id],
+                            current.cost + d, c_id)
+
+                if n_id in closed_set:
+                    continue
+                # Otherwise if it is already in the open set
+                if n_id in open_set:
+                    if open_set[n_id].cost > node.cost:
+                        open_set[n_id].cost = node.cost
+                        open_set[n_id].parent_index = c_id
+                else:
+                    open_set[n_id] = node
+
+        if path_found is False:
+            if(verbiose):
+                print("Path not found")
+            return [], []
+        else:
+            if(verbiose):
+                print("Path found")
+        # generate final course
+        rx, ry = [goal_node.x], [goal_node.y]
+        parent_index = goal_node.parent_index
+        while parent_index != -1:
+            n = closed_set[parent_index]
+            rx.append(n.x)
+            ry.append(n.y)
+            parent_index = n.parent_index
+
+        return rx, ry
+
+
+    @staticmethod
+    def calcHeuristic(n1, n2):
+        w = 1.0  # weight of heuristic
+        d = w * math.hypot(n1.x - n2.x, n1.y - n2.y)
+        return d
+
+    def AStar(self, road_map, sample_x, sample_y, verbiose=True):
+
+        goal_node = Node(self._gx, self._gy, 0.0, -1)
+        start_node = Node(self._sx, self._sy, 0.0, -1)
+
+        open_set, closed_set = dict(), dict()
+        open_set[len(road_map) - 2] = start_node
+        path_found = True
+
+        while True:
+            if not open_set:
+                path_found = False
+                break
+
+            c_id = min(open_set, key=lambda o: open_set[o].cost + self.calcHeuristic(goal_node, open_set[o]))
             current = open_set[c_id]
 
             if c_id == (len(road_map) - 1):
@@ -173,8 +263,12 @@ class PRM:
                     open_set[n_id] = node
 
         if path_found is False:
+            if(verbiose):
+                print("Path not found")
             return [], []
-
+        else:
+            if( verbiose):
+                print("Path found")
         # generate final course
         rx, ry = [goal_node.x], [goal_node.y]
         parent_index = goal_node.parent_index
@@ -183,8 +277,11 @@ class PRM:
             rx.append(n.x)
             ry.append(n.y)
             parent_index = n.parent_index
+        
 
         return rx, ry
+
+
 
     def generateSamplePoints(self, ox, oy, obstacle_kd_tree):
         max_x = max(ox)
@@ -248,7 +345,6 @@ class PRM:
         for i in range(6):  # 3 seconds of wait
             video.write(imageStart)
 
-        
         if(rx != None):
             imageSubsequent = imageStart
             for i in range(len(rx)-2, 0, -1):
@@ -286,7 +382,7 @@ class PRM:
 
 if __name__ == "__main__":
 
-    img = cv2.imread(__file__.replace("prm.py", "input2.png"), 0)
+    img = cv2.imread(__file__.replace("PRM.py", "map.png"), 0)
     res = 20.0
     sx = 16.6
     sy = 1.6
@@ -294,7 +390,18 @@ if __name__ == "__main__":
     gy = 220.0 / 20  # [m]
     robotSize = 0.160 / 2  # [m]
 
+    import time
     test = PRM(img, res, sx, sy, gx, gy, robotSize)
-    rx, ry = test.startPlanner()
-    test.saveToVideo(rx, ry, True)
 
+    start = time.clock()
+    print("AStar starting")
+    print(" ")
+    rx, ry = test.startPlanner()
+    #print("Elapsed time 1: " + str(time.clock()- start))
+
+    start = time.clock()
+    print("Dijkstra starting")
+    print(" ")
+    rx, ry = test.startPlanner(False)
+    #print("Elapsed time 2: " + str(time.clock()- start))
+    #test.saveToVideo(rx, ry, True)
